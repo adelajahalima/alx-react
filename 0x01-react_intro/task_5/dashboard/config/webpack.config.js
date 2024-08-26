7f7b7a347ad67d72e071a432e93b64f7abad4050
adelajahalima <adelajahalima@gmail.com>
2024-08-26 20:46:24 +0100

    Edited webpack.config.js

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -2,42 +2,35 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './dashboard/src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true, // Clean the output directory before emit
+  entry: path.resolve(__dirname, '../src/index.js'), // Entry point is in the src folder
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'), // Output goes to the dist folder
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, '../dist'),
     },
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-        hot: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-                use: [
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            bypassOnDebug: true, // Bypass in development
-                            disable: true,
-                        },
-                    },
-                ],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './dashboard/dist/index.html',
-        }),
+    hot: true, // Enable hot reloading
+    compress: true,
+    port: 9000, // You can change the port if needed
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'], // For handling CSS files
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        type: 'asset/resource', // For handling image files
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../dist/index.html'), // Template for the HTML file
+    }),
+  ],
+  devtool: 'inline-source-map', // For inline source maps
 };
